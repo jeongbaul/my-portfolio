2dce45cbf5f29dbebc8253395e5da039c23412b0
joonhyeok95 <dkttkemf@gmail.com>
2025-10-29 21:19:00 +0900

    htaccess 경로잡는 연습
    header, footer, skill 등

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer-->
+<footer class="footer text-center">
+    <div class="container px-4 px-lg-5">
+        <p class="text-muted small mb-0">Copyright &copy; Jeong Baul Portfolio 2025</p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/scripts.js"></script>
+
+</body>
+</html>
